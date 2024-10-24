10d4bd5c25ff76a8b8e73a811224e7fe7f8a8a6c
Arjun Singh <136841219+ArjunSingh913@users.noreply.github.com>
2024-10-24 15:44:19 +0530

    feat(debloyment): deplying on server

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,14 @@ const config: Config = {
   content: [
     './src/**/*.{js,ts,jsx,tsx}', // All source files
     './public/**/*.html', // HTML files in the public directory
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {}, // Extend default theme here
   },
   plugins: [], // Add any Tailwind plugins if needed
+  purge: false,
 };
 
 export default config;
